9ca7586c90dfd81edb6d3912408955a4b66ec484
Brian Joerger <joergerbrian@gmail.com>
2019-04-04 15:06:51 -0700

    Move GeneralStore instantiation to server.go with root resolver instantiation function GetRootResolver.

--- server/resolver/message.go
+++ server/resolver/message.go
@@ -2,27 +2,31 @@ package resolver
 
 import (
 	"circles/server/model"
-	"circles/server/store"
 	"circles/server/types"
 	"context"
 )
 
 // CreateMessage creates a new Message with the given data
-func (r *Resolver) CreateMessage(ctx context.Context, args *struct{ Input *types.Message }) (*model.MessageModel, error) {
-	return model.CreateMessage(ctx, &store.GeneralStore{}, args.Input)
+func (r *Resolver) CreateMessage(ctx context.Context, args *struct{ Message *types.Message }) (*model.MessageModel, error) {
+	return model.CreateMessage(ctx, r.store, args.Message)
 }
 
 // DeleteMessage deletes the user specified by ID
 func (r *Resolver) DeleteMessage(ctx context.Context, args *IDArgs) (*model.MessageModel, error) {
-	return model.DeleteMessage(ctx, &store.GeneralStore{}, args.ID)
+	return model.DeleteMessage(ctx, r.store, args.ID)
 }
 
 // Message retrieves the user specified by id
 func (r *Resolver) Message(ctx context.Context, args *IDArgs) (*model.MessageModel, error) {
-	return model.FindMessage(ctx, &store.GeneralStore{}, args.ID)
+	return model.FindMessage(ctx, r.store, args.ID)
+}
+
+// Messages retrieves all users
+func (r *Resolver) Messages(ctx context.Context) ([]*model.MessageModel, error) {
+	return model.AllMessages(ctx, r.store)
 }
 
 // UpdateMessage updates the user specified by ID with the given data
-func (r *Resolver) UpdateMessage(ctx context.Context, args *struct{ Input *model.UpdateMessageInput }) (*model.MessageModel, error) {
-	return model.UpdateMessage(ctx, &store.GeneralStore{}, args.Input)
+func (r *Resolver) UpdateMessage(ctx context.Context, args *struct{ Message *model.UpdateMessageInput }) (*model.MessageModel, error) {
+	return model.UpdateMessage(ctx, r.store, args.Message)
 }
